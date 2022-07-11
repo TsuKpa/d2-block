c7ac8823f92811e3a49c115b1d4a56a2518bbb17
tsukpa <tsunamikampa@gmail.com>
2022-07-11 08:42:19 +0700

    add modal ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,6 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { HomeModule } from './home/home.module';
-import { DetailModule } from './detail/detail.module';
-
 import { AppComponent } from './app.component';
 
 // AoT requires an exported function for factories
@@ -28,7 +26,6 @@ const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new Transl
     CoreModule,
     SharedModule,
     HomeModule,
-    DetailModule,
     AppRoutingModule,
     TranslateModule.forRoot({
       loader: {
